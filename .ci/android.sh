32ac0b5d1d7bfe228af037cb7452323c3df6ac0b
freestyleabg <freestyleabg@gmail.com>
2026-04-15 16:56:15 +0100

    CI: opt into Node 24; build vanilla Android

--- .ci/android.sh
+++ .ci/android.sh
@@ -9,8 +9,18 @@ fi
 
 cd src/android
 chmod +x ./gradlew
-./gradlew assembleRelease
-./gradlew bundleRelease
+
+# By default, build only the "vanilla" flavor to avoid Google Play flavor CI breakages.
+# Set ANDROID_BUILD_ALL_FLAVORS=1 to restore the old behavior (assembleRelease/bundleRelease).
+if [ "${ANDROID_BUILD_ALL_FLAVORS:-0}" = "1" ]; then
+    ./gradlew assembleRelease --stacktrace
+    ./gradlew bundleRelease --stacktrace
+else
+    FLAVOR="${ANDROID_FLAVOR:-vanilla}"
+    BUILD_TYPE="${ANDROID_BUILD_TYPE:-Release}"
+    ./gradlew "assemble${FLAVOR^}${BUILD_TYPE}" --stacktrace
+    ./gradlew "bundle${FLAVOR^}${BUILD_TYPE}" --stacktrace
+fi
 
 ccache -s -v
 
